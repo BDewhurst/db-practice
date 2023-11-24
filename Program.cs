54ea4966fe590ddc3ea1c0071dbadd1b2d232a95
Brad <bradleydewhurst123@outlook.com>
2023-11-24 15:04:44 +0000

    DB practice start

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DnsClient.Protocol;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Connections;
+using MongoDBDemo;
+
+string connectionString = "";
+string databaseName = "simple_db";
+string collectionsName = "people";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<PersonModel>(collectionsName);
+
+var person = new PersonModel {FirstName ="Tim", LastName = "Corey"};
+await collection.InsertOneAsync(person);
+var results = await collection.FindAsync(_ => true);
+foreach ( var result in results.ToList()) {
+Console.WriteLine($"{result.Id} : {result.FirstName} {result.LastName}");
+}
+
